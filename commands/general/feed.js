cfaa850d792685a9abce63611de315747034155e
Tyler Maldonado <tylermaldonado747@gmail.com>
2019-07-13 06:27:39 -0400

    setup for coding the feed command

--- commands/general/feed.js
+++ commands/general/feed.js
@@ -7,6 +7,13 @@ module.exports.run = async (bot, message, args) => {
   .setColor(config.color.cats)
   .setDescription('this command doesn\'t do anything currently\nif you have an idea for this command use `cat suggest` to let me know');
   message.channel.send(newCommandEmbed);
+
+  /* User suggestion
+    Maybe do [cat feed] then insert a cat breed then there will be a chance
+    you'll get the cat. The chance varies depending on how high the cat is
+    from looking in [cat collection]
+  */
+
 }
 
 module.exports.help = {
