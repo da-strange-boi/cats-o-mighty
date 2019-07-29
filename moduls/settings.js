24c89ea539f5399f8dc933daba60f36d36bbd5d2
Tyler Maldonado <tylermaldonado747@gmail.com>
2019-07-29 03:35:37 -0400

    try 2

--- moduls/settings.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const settingsSchema = mongoose.Schema({
-  guildID: String,
-  showCat: Boolean
-});
-
-module.exports = mongoose.model("guildSettings", settingsSchema);
