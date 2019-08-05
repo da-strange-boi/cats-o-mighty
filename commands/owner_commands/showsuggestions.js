7acbf205be80fed850d187bf3d72f09850775cdd
Tyler Maldonado <tylermaldonado747@gmail.com>
2019-08-05 18:56:05 -0400

    fixed showsuggestions as only 25 fields can be displayed

--- commands/owner_commands/showsuggestions.js
+++ commands/owner_commands/showsuggestions.js
@@ -7,39 +7,46 @@ const Suggestion = require("../../moduls/suggestions.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if(message.author.id != "295255543596187650"){ 
-        return; 
-    }else if (message.author.id === "295255543596187650") {
-        //* Select User Data From Database
-        Suggestion.find({}, (err, res) => {
-            if(err) console.log(err);
-            if(!res){
-                let noSuggestionsEmbed = new Discord.RichEmbed()
-                .setColor(config.color.error)
-                .setDescription("There are no suggestions :(");
-                message.channel.send(noSuggestionsEmbed);
-                return;
+  if(message.author.id != "295255543596187650"){ 
+      return; 
+  }else if (message.author.id === "295255543596187650") {
+    //* Select User Data From Database
+    Suggestion.find({}, (err, res) => {
+      if(err) console.log(err);
+      if(!res){
+        let noSuggestionsEmbed = new Discord.RichEmbed()
+        .setColor(config.color.error)
+        .setDescription("There are no suggestions :(");
+        message.channel.send(noSuggestionsEmbed);
+        return;
+      }
+      if(res){
+        let embed = new Discord.RichEmbed()
+        .setTitle("**Suggestions List**")
+        
+        u = 0;
+        //* If There Are No Results
+        if(res.length === 0){
+          embed.setColor(config.color.error);
+          embed.addField("No data found", "No one has suggesed anything");
+            
+        } else {
+          embed.setColor(config.color.owner);
+          if(res.length > 25){
+            for(i = 0; i < 25; i++){
+              embed.addField(`${res[i].userTag} (${res[i].userID}) || #${res[i].suggestionNumber}`, `${res[i].suggestion}`);
             }
-            if(res){
-                let embed = new Discord.RichEmbed()
-                .setTitle("**Suggestions List**")
-                
-                u = 0;
-                //* If There Are No Results
-                if(res.length === 0){
-                    embed.setColor(config.color.error);
-                    embed.addField("No data found", "No one has suggesed anything")
-                    
-                } else {
-                    embed.setColor(config.color.owner);
-                    for(i = 0; i < res.length; i++) {
-                        embed.addField(`${res[i].userTag} (${res[i].userID}) || #${res[i].suggestionNumber}`, `${res[i].suggestion}`);
-                    }
-                }
-                message.channel.send(embed);
+            embed.setFooter(`only 25 out of ${res.length} displayed`);
+          } else {
+             for(i = 0; i < res.length; i++) {
+              embed.addField(`${res[i].userTag} (${res[i].userID}) || #${res[i].suggestionNumber}`, `${res[i].suggestion}`);
             }
-        });
-    }
+          }
+        }
+        message.channel.send(embed);
+      }
+    });
+  }
 }
 
 module.exports.help = {
