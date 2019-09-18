c3a3b67d62d691e28ac01f6bcbd90b44158b960b
Tyler <tylersmaldonado@gmail.com>
2019-09-17 20:03:11 -0400

    f this

--- bot/handlers/processCommand.js
+++ bot/handlers/processCommand.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 exports.run = async (bot, message, cmd, args, prefix) => {
 
+  if(!message.content.trim().toLowerCase().startsWith(prefix)) return;
+
   if(cmd.help.name === `start`){
     //* If The User Is A New Cat Collector And Runs 'cat start'
     bot.db.Userdata.findOne({userID: message.author.id}, async (err, userdata) => {
