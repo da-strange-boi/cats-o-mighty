20e0149e76cc4a19e07b4fd353d9b80e0cf14f8f
Tyler <tylersmaldonado@gmail.com>
2019-09-22 15:39:00 -0400

    little vote message

--- bot/commands/vote.js
+++ bot/commands/vote.js
@@ -11,7 +11,7 @@ exports.run = async (bot, message, args) => {
     if(err) console.log(err);
 
     if(!userdata){
-      message.channel.send(`:ballot_box_with_check: **You can claim your bi-daily vote now!**\n        You can vote every 12 hours\nhttps://discordbots.org/bot/569336139186700312/vote`);
+      message.channel.send(`:ballot_box_with_check: **You can claim your bi-daily vote now!**\n        You can vote every 12 hours\nhttps://discordbots.org/bot/569336139186700312/vote\n\nIf you could also vote for the bot at https://botsfordiscord.com/bot/569336139186700312/vote you won't get any rewards but it will be greatly appreciated, thanks!`);
       return;
     }
 
@@ -20,9 +20,9 @@ exports.run = async (bot, message, args) => {
 
     if(vote !== null && timeout - (Date.now() - vote) > 0){
       let time = ms(timeout - (Date.now() - vote));
-      message.channel.send(`You can vote every 12 hours\nIn **${time.hours}h ${time.minutes}m ${time.seconds}s** you can vote again\nhttps://discordbots.org/bot/569336139186700312/vote`);
+      message.channel.send(`You can vote every 12 hours\nIn **${time.hours}h ${time.minutes}m ${time.seconds}s** you can vote again\nhttps://discordbots.org/bot/569336139186700312/vote\n\nIf you could also vote for the bot at https://botsfordiscord.com/bot/569336139186700312/vote you won't get any rewards but it will be greatly appreciated, thanks!`);
     } else {
-      message.channel.send(`:ballot_box_with_check: **You can claim your bi-daily vote now!**\n        You can vote every 12 hours\nhttps://discordbots.org/bot/569336139186700312/vote`);
+      message.channel.send(`:ballot_box_with_check: **You can claim your bi-daily vote now!**\n        You can vote every 12 hours\nhttps://discordbots.org/bot/569336139186700312/vote\n\nIf you could also vote for the bot at https://botsfordiscord.com/bot/569336139186700312/vote you won't get any rewards but it will be greatly appreciated, thanks!`);
     }
   });
 }
