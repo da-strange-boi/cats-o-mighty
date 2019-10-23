7732b5eae0e101fd49d8e12bbc172e8aae9dbcf2
Tyler <tylersmaldonado@gmail.com>
2019-10-23 03:01:35 -0400

    cleaning up grandma

--- bot/lib/checkCats.js
+++ bot/lib/checkCats.js
@@ -2,52 +2,30 @@
   this is just all one big mess that probably shouldn't be messed with
   unless a cat is being added or this method is being changed :)
 
+  - (edit 10/23/19) its not that messey now that i used a for loop and not just a bunch of if statements
+
   all this does is check if a user has all the updated cats in their
   database userdata otherwise the data would be undefined
 */
+const newCatList = [
+  'cursedcat', 'russianblue', 'munchkin', 'turkishangora', 'loki', 'loverboy', 'uwu', 
+  'calico', 'tabby', 'norwegianforest', 'britishshorthair', 'tom', 'demoncat', 'killerclaws', 
+  'devonrex', 'ojosazules', 'bongocat', 'grumpycat', 'ghostcat'
+]
 exports.run = (bot, message) => {
   // Select A User Data From The Database
   bot.database.Userdata.findOne({ userID: message.author.id }, (err, userData) => {
     if (err) bot.log('error', err)
     if (userData) {
       const userCatAmt = userData.cats
-      if (userCatAmt.cursedcat === undefined) {
-        userCatAmt.cursedcat = 0
+
+      for (let i = 0; i < newCatList.length; i++) {
+        // if any values (of new cats) returns undefined (non existent) then add it and give it a value of 0
+        if (userCatAmt[newCatList[i]] === undefined) {
+          userCatAmt[newCatList[i]] = 0
+        }
       }
-      if (userCatAmt.russianblue === undefined && userCatAmt.munchkin === undefined) {
-        userCatAmt.russianblue = 0
-        userCatAmt.munchkin = 0
-      }
-      if (userCatAmt.turkishangora === undefined) {
-        userCatAmt.turkishangora = 0
-      }
-      if (userCatAmt.loki === undefined && userCatAmt.loverboy === undefined) {
-        userCatAmt.loki = 0
-        userCatAmt.loverboy = 0
-      }
-      if (userCatAmt.uwu === undefined) {
-        userCatAmt.uwu = 0
-      }
-      if (userCatAmt.calico === undefined && userCatAmt.tabby === undefined && userCatAmt.norwegianforest === undefined && userCatAmt.britishshorthair === undefined && userCatAmt.tom === undefined && userCatAmt.demoncat === undefined) {
-        userCatAmt.calico = 0
-        userCatAmt.tabby = 0
-        userCatAmt.norwegianforest = 0
-        userCatAmt.britishshorthair = 0
-        userCatAmt.tom = 0
-        userCatAmt.demoncat = 0
-      }
-      if (userCatAmt.killerclaws === undefined) {
-        userCatAmt.killerclaws = 0
-      }
-      if (userCatAmt.devonrex === undefined && userCatAmt.ojosazules === undefined && userCatAmt.bongocat === undefined && userCatAmt.grumpycat === undefined) {
-        userCatAmt.devonrex = 0
-        userCatAmt.ojosazules = 0
-        userCatAmt.bongocat = 0
-        userCatAmt.grumpycat = 0
-      }
-      if (userCatAmt.ghostcat === undefined) {
-        userCatAmt.ghostcat = 0
-      }
+
       userData.userTag = message.author.tag
       userData.save().catch(err => console.log(err))
     }
--- bot/lib/logging.js
+++ bot/lib/logging.js
@@ -7,6 +7,11 @@ const getCurrentDate = () => {
   return getDate.run(date)
 }
 
+/**
+ * Logs a message to the console
+ * @param {string} logType system, error, statsPosted
+ * @param {string} logMessage whatever the log should say
+ */
 const logging = async function Log (logType, logMessage) {
   switch (logType) {
     case 'system': console.log(chalk.inverse('[LOG][SYSTEM]') + chalk.hex(config.color.white)(` ${logMessage} `) + chalk.blue(getCurrentDate()))
