72b709c8ddb03ac8463e78d42d0eddef3cc6f5ac
Ben <benluelo@hotmail.com>
2019-11-23 17:07:54 -0500

    started new collection command7

---
+++ bot/commands/col2.js
@@ -0,0 +1,56 @@
+const MongoClient = require('mongodb').MongoClient
+const config = require('./config')
+const Discord = require('discord.js').RichEmbed
+
+exports.run = async (bot, message, args) => {
+  MongoClient.connect(config.db.connectionString, config.db.connectionOptions, (err, client) => {
+
+    const col2Embed = new Discord.RichEmbed()
+      .setTitle('Collection 2')
+
+    if (err) throw err
+      
+    let collection = client.db('cats-o-mighty').collection('userdatas')
+
+    collection.findOne({ userID: '527729016849956874' /* message.author.id */ }, (err, userdata) => {
+
+      if(err) throw err
+
+      const rarities = Object.keys(userdata.cats) // array containing all the keys (rarities) of userdata.cats
+    
+      // loop through rarities
+      for(let rarity in rarities){
+
+        // define blank string to be added on to
+        let rarityField = ''
+
+        const o = Object.keys(userdata.cats[rarities[rarity]]) // array containing all the keys (cats) of userdata.cats.{rarity}
+
+        // loop through the cats in each rarity
+        for(let i in o){
+          rarityField += catProcess(userdata.cats[rarities[rarity]][o[i]], o[i])
+        }
+
+        // add rarity field to embed
+        col2Embed.addField(rarities[rarity], rarityField)
+      }
+
+      // finally, send the embed
+      message.channel.send(col2Embed)
+    })
+  })
+}
+
+exports.help = {
+  name: 'col2',
+  aliases: ['cattos2', 'c2', 'tos2'],
+  type: 'normal'
+}
+
+const catProcess = (catObject, catName) => {
+  if (catObject.discovered === false) {
+    return '?'.repeat(catName.length)
+  } else {
+    return `${catName.charAt(0).toUpperCase() + catName.slice(1)}: **\`${catObject.amount}\`\n**`
+  }
+}
---
+++ dbUpdate.js
@@ -0,0 +1,154 @@
+const MongoClient = require('mongodb').MongoClient
+const config = require('./bot/config.json')
+
+MongoClient.connect(config.db.connectionString, config.db.connectionOptions, (err, client) => {
+  if (err) throw err
+
+  let collection = client.db('cats-o-mighty').collection('userdatas')
+
+  let documentsModified_SET = 0
+
+  collection.find({}).forEach(element => {
+    collection.updateOne({'_id': element._id},
+      {
+        $set: {
+          'cats.common': {
+            siamese: element.cats.siamese,
+            burmese: element.cats.burmese,
+            ragdoll: element.cats.ragdoll,
+            persian: element.cats.persian,
+            mainecoon: element.cats.mainecoon,
+            russianblue: element.cats.russianblue,
+            calico: element.cats.calico,
+            tabby: element.cats.tabby
+          },
+          'cats.uncommon': {
+            abyssinian: element.cats.abyssinian,
+            manx: element.cats.manx,
+            sphynx: element.cats.sphynx,
+            cyprus: element.cats.cyprus,
+            foldex: element.cats.foldex,
+            turkishangora: element.cats.turkishangora,
+            norwegianforest: element.cats.norwegianforest,
+            devonrex: element.cats.devonrex
+          },
+          'cats.rare': {
+            korat: element.cats.korat,
+            singapura: element.cats.singapura,
+            tonkinese: element.cats.tonkinese,
+            peterbald: element.cats.peterbald,
+            chartreux: element.cats.chartreux,
+            munchkin: element.cats.munchkin,
+            britishshorthair: element.cats.britishshorthair,
+            ojosazules: element.cats.ojosazules
+          },
+          'cats.special': {
+            smokey: element.cats.smokey,
+            bandit: element.cats.bandit,
+            bug: element.cats.bug,
+            linda: element.cats.linda,
+            mittens: element.cats.mittens,
+            cash: element.cats.cash,
+            jackson: element.cats.jackson,
+            cottonball: element.cats.cottonball,
+            sonny: element.cats.sonny,
+            lailah: element.cats.lailah,
+            cher: element.cats.cher,
+            marvin: element.cats.marvin,
+            loki: element.cats.loki,
+            loverboy: element.cats.loverboy,
+            killerclaws: element.cats.killerclaws
+          },
+          'cats.impossible': {
+            squirtlett: element.cats.squirtlett,
+            cursedcat: element.cats.cursedcat,
+            uwu: element.cats.uwu,
+            tom: element.cats.tom,
+            demoncat: element.cats.demoncat,
+            bongocat: element.cats.bongocat,
+            grumpycat: element.cats.grumpycat
+          },
+          'cats.seasonal': {
+            ghostcat: element.cats.ghostcat
+          }
+        }
+      },
+      (err, res) => {
+        if (err) throw err
+        documentsModified_SET += parseInt(res.result.nModified)
+      }
+    )
+    // console.log(element.cats)
+  })
+  console.log(documentsModified_SET)
+
+  collection.updateMany({},
+    {
+      $unset: {
+        // common
+        'cats.siamese': 1,
+        'cats.burmese': 1,
+        'cats.ragdoll': 1,
+        'cats.persian': 1,
+        'cats.mainecoon': 1,
+        'cats.russianblue': 1,
+        'cats.calico': 1,
+        'cats.tabby': 1,
+
+        // uncommon
+        'cats.abyssinian': 1,
+        'cats.manx': 1,
+        'cats.sphynx': 1,
+        'cats.cyprus': 1,
+        'cats.foldex': 1,
+        'cats.turkishangora': 1,
+        'cats.norwegianforest': 1,
+        'cats.devonrex': 1,
+
+        // rare
+        'cats.korat': 1,
+        'cats.singapura': 1,
+        'cats.tonkinese': 1,
+        'cats.peterbald': 1,
+        'cats.chartreux': 1,
+        'cats.munchkin': 1,
+        'cats.britishshorthair': 1,
+        'cats.ojosazules': 1,
+
+        // special
+        'cats.smokey': 1,
+        'cats.bandit': 1,
+        'cats.bug': 1,
+        'cats.linda': 1,
+        'cats.mittens': 1,
+        'cats.cash': 1,
+        'cats.jackson': 1,
+        'cats.cottonball': 1,
+        'cats.sonny': 1,
+        'cats.lailah': 1,
+        'cats.cher': 1,
+        'cats.marvin': 1,
+        'cats.loki': 1,
+        'cats.loverboy': 1,
+        'cats.killerclaws': 1,
+
+        // impossible
+        'cats.squirtlett': 1,
+        'cats.cursedcat': 1,
+        'cats.uwu': 1,
+        'cats.tom': 1,
+        'cats.demoncat': 1,
+        'cats.bongocat': 1,
+        'cats.grumpycat': 1,
+
+        // seasonal
+        'cats.ghostcat': 1
+      }
+    },
+    (err, res) => {
+      if (err) throw err
+      console.log(res.result.nModified, 'Documents modified ($unset)')
+    }
+  )
+  console.log()
+})
