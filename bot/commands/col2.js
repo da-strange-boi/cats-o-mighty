751222a33c813ceaed8d93d69429f90c0e4367cd
Tyler <tylersmaldonado@gmail.com>
2019-11-27 00:07:02 -0500

    yee

--- bot/commands/col2.js
+++ bot/commands/col2.js
@@ -2,28 +2,30 @@ const MongoClient = require('mongodb').MongoClient
 const config = require('../config')
 const RichEmbed = require('discord.js').RichEmbed
 
+const catProcess = (catObject, catName) => {
+  if (catObject.discovered === false) {
+    return ('?'.repeat(catName.length) + '\n')
+  } else {
+    return `${catName.charAt(0).toUpperCase() + catName.slice(1)}: **\`${catObject.amount}\`\n**`
+  }
+}
+
 exports.run = async (bot, message, args) => {
-  MongoClient.connect(config.db.connectionString, config.db.connectionOptions, (err, client) => {
 
-    const col2Embed = new RichEmbed()
-      .setTitle('Collection 2')
+  const col2Embed = new RichEmbed()
+    .setTitle('Collection 2')
+    .setColor(bot.config.color.blue)
 
+  bot.database.Userdata.findOne({ userID: message.author.id  }, (err, userdata) => {
     if (err) throw err
-      
-    let collection = client.db('cats-o-mighty').collection('userdatas')
 
-    collection.findOne({ userID: '527729016849956874' /* message.author.id */ }, (err, userdata) => {
-
-      if(err) throw err
-
+    if (userdata) {
       const rarities = Object.keys(userdata.cats) // array containing all the keys (rarities) of userdata.cats
     
       // loop through rarities
       for(let rarity in rarities){
 
-        // define blank string to be added on to
         let rarityField = ''
-
         const o = Object.keys(userdata.cats[rarities[rarity]]) // array containing all the keys (cats) of userdata.cats.{rarity}
 
         // loop through the cats in each rarity
@@ -32,12 +34,12 @@ exports.run = async (bot, message, args) => {
         }
 
         // add rarity field to embed
-        col2Embed.addField(rarities[rarity].slice(2), rarityField, true)
+        col2Embed.addField(`${bot.functions.cap(rarities[rarity].slice(2))}`, rarityField, true)
       }
 
       // finally, send the embed
       message.channel.send(col2Embed)
-    })
+    }
   })
 }
 
@@ -45,12 +47,4 @@ exports.help = {
   name: 'col2',
   aliases: ['cattos2', 'c2', 'tos2'],
   type: 'normal'
-}
-
-const catProcess = (catObject, catName) => {
-  if (catObject.discovered === false) {
-    return ('?'.repeat(catName.length) + '\n')
-  } else {
-    return `${catName.charAt(0).toUpperCase() + catName.slice(1)}: **\`${catObject.amount}\`\n**`
-  }
 }
